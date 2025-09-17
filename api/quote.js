67d6015a4627341748cb7567c836c7972a35a7e2
Daniel <dh.negocios01@gmail.com>
2025-09-17 18:32:32 -0400

    Integra alias de toda La Altagracia y cálculo de tarifas

--- api/quote.js
+++ api/quote.js
@@ -1,4 +1,4 @@
-import aliasZonas from "../data/aliasZonas.js";
+import aliasZonas from "../Data/aliaszonas.js";
 
 export default function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
